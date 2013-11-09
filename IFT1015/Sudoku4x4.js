ff29b7f9e45b6ad8d6b5bf5ad193e577258ae5ab
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-11-09 16:39:26 -0500

    fixed errors in printing

--- IFT1015/Sudoku4x4.js
+++ IFT1015/Sudoku4x4.js
@@ -6,7 +6,7 @@
 //  
 //////////////////////////////////////////////////////////////
 
-load"http://www-etud.iro.umontreal.ca/~dridihou/sudokus.js";
+load("http://www-etud.iro.umontreal.ca/~dridihou/sudokus.js");
 
 // Generates a random number between 0 and 30
 var num = Math.floor(Math.random() *30);
@@ -17,11 +17,11 @@ var ecran = function(t)
 	// Need to add an assert for the parameter of the function
 	
 	// Prints the sodoku to the command line
-	print(" 1 2 3 4 \n ----------- 	\n a | " + t[0][0] + " " + t[0][1]) + " | " + t[0][2] + " " + t[0][3] = " | \n"
-	+ " b | " + t[1][0] + " " + t[1][1]) + " | " + t[1][2] + " " + t[1][3] = " | \n"
+	print(" 1 2 3 4 \n ----------- 	\n a | " + t[0][0] + " " + t[0][1]) + " | " + t[0][2] + " " + t[0][3] + " | \n"
+	+ " b | " + t[1][0] + " " + t[1][1] + " | " + t[1][2] + " " + t[1][3] + " | \n"
     + "   | ----- | ----- | \n "
-	+ " c | " + t[2][0] + " " + t[2][1]) + " | " + t[2][2] + " " + t[2][3] = " | \n" 
-    + " d | " + t[3][0] + " " + t[3][1]) + " | " + t[3][2] + " " + t[3][3] = " | \n" 
+	+ " c | " + t[2][0] + " " + t[2][1] + " | " + t[2][2] + " " + t[2][3] + " | \n" 
+    + " d | " + t[3][0] + " " + t[3][1] + " | " + t[3][2] + " " + t[3][3] + " | \n" 
     + " ----------- ";
 
 };
