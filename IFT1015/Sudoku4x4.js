90a449f487d59d440ac97b64dc06d1293997eb08
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-11-09 16:24:47 -0500

    Modified ecran function

--- IFT1015/Sudoku4x4.js
+++ IFT1015/Sudoku4x4.js
@@ -8,19 +8,21 @@
 
 load"http://www-etud.iro.umontreal.ca/~dridihou/sudokus.js";
 
-
-// Affiche l'etat actual du jeu de sodoku a la ligne de command
-
 // Generates a random number between 0 and 30
-var num = Math.floor(Math.random *30);
-	
+var num = Math.floor(Math.random() *30);
+
+// Affiche l'etat actual du jeu de sodoku a la ligne de command	
 var ecran = function(t)
 {
 	// Need to add an assert for the parameter of the function
 	
-	
 	// Prints the sodoku to the command line
-	print("");
+	print(" 1 2 3 4 \n ----------- 	\n a | " + t[0][0] + " " + t[0][1]) + " | " + t[0][2] + " " + t[0][3] = " | \n"
+	+ " b | " + t[1][0] + " " + t[1][1]) + " | " + t[1][2] + " " + t[1][3] = " | \n"
+    + "   | ----- | ----- | \n "
+	+ " c | " + t[2][0] + " " + t[2][1]) + " | " + t[2][2] + " " + t[2][3] = " | \n" 
+    + " d | " + t[3][0] + " " + t[3][1]) + " | " + t[3][2] + " " + t[3][3] = " | \n" 
+    + " ----------- ";
 
 };
 
