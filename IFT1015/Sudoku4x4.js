b17be44fa9e4fc7eea248f40353f6a22edb8daaf
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-11-09 17:10:31 -0500

    Fixed bug ecran function

--- IFT1015/Sudoku4x4.js
+++ IFT1015/Sudoku4x4.js
@@ -9,25 +9,23 @@
 load("http://www-etud.iro.umontreal.ca/~dridihou/sudokus.js");
 
 // Generates a random number between 0 and 30
-var num = Math.floor(Math.random() *30);
+var t = sudoku [Math.floor(Math.random()*30)];
 
 // Affiche l'etat actual du jeu de sodoku a la ligne de command	
-var ecran = function(t)
+var ecran = function()
 {
 	// Need to add an assert for the parameter of the function
-	
-	// Prints the sodoku to the command line
-	print(	" 1 2 3 4 \n ----------- 	\n a | " + t[0][0] + " " + t[0][1] + " | " + t[0][2] + " " + t[0][3] + " | \n"
-	+ " b | " + t[1][0] + " " + t[1][1] + " | " + t[1][2] + " " + t[1][3] + " | \n"
-    + "   | ----- | ----- | \n "
-	+ " c | " + t[2][0] + " " + t[2][1] + " | " + t[2][2] + " " + t[2][3] + " | \n" 
-    + " d | " + t[3][0] + " " + t[3][1] + " | " + t[3][2] + " " + t[3][3] + " | \n" 
-    + " ----------- ");
+    print (("    1 2   3 4  \n   -----------  \na | "
+	+t[0][0]+" "+t[0][1]+" | "+t[0][2]+" "+t[0][3]+" |")+
+	("\nb | "+t[1][0]+" "+t[1][1]+" | "+t[1][2]+" "+t[1][3]+" |")+
+	("\n  |-----|-----|")+
+	("\nc | "+t[2][0]+" "+t[2][1]+" | "+t[2][2]+" "+t[2][3]+" |")+
+	("\nd | "+t[3][0]+" "+t[3][1]+" | "+t[3][2]+" "+t[3][3]+" |\n" + "   -----------   "));
 
 };
 
 // Calls the function passing the random number as a parameter
-ecran(sodoku[num]);
+ecran(t);
 
 // Doing some test functions
 var test = function(r)
