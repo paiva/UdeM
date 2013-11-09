e64c750326159a22de8f0932480483d707b25715
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-11-09 16:05:53 -0500

    modified ecran function

--- IFT1015/Sudoku4x4.js
+++ IFT1015/Sudoku4x4.js
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////
 //
-// Devoir 1: Sododku.js
+// Devoir 1: Sodoku.js
 // Auteurs: Olga Prezh (ID) et Santiago Paiva (1066417)
 // Date: 21 Novembre 2013
 //  
@@ -10,17 +10,24 @@ load"http://www-etud.iro.umontreal.ca/~dridihou/sudokus.js";
 
 
 // Affiche l'etat actual du jeu de sodoku a la ligne de command
+
+// Generates a random number between 0 and 30
+var num = Math.floor(Math.random *30);
+	
 var ecran = function(t)
 {
-	// Generate a random number between 0 and 30
-	var num = Math.floor(Math.random *30);
-	print();
+	// Need to add an assert for the parameter of the function
+	
+	
+	// Prints the sodoku to the command line
+	print("");
 
 };
 
-// Call the function passing the random number as a parameter
+// Calls the function passing the random number as a parameter
 ecran(sodoku[num]);
 
+// Doing some test functions
 var test = function(r)
 {
  ecran(sodoku[0]);
