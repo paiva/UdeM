8db5f2fa7a3e6b211c9c040182523bb41730f36d
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-12-07 23:23:12 -0500

    added constructors

--- IFT1015/droite.js
+++ IFT1015/droite.js
@@ -1,5 +1,28 @@
-///////////////////////////////
+//////////////////////////////////////////
 //
-//
-//
-////////////////////////
+// Ex 7 : OOP 
+// Auteur: Santiago Paiva (1066417)
+// Date de remise: Dec 12, 2013
+// 
+//////////////////////////////////////////
+
+
+// Constructeur pour Droite
+var Droite = function(a,b)
+{
+
+};
+
+// Constructeur pour Point
+var Droite = function(a,b)
+{
+
+};
+
+
+
+
+
+
+
+
